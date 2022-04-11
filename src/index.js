da5ed466439291360b8b179d1d21b256cf370dc9
S1791 <kimberntson@outlook.com>
2022-04-11 20:56:34 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+//jshint esversion:6
+
+function magicAnswer() {
+  const eightBallArray = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+  ];
+
+  let result = eightBallArray[Math.floor(Math.random() * 20)];
+  document.getElementById("answer").innerHTML = result;
+}
